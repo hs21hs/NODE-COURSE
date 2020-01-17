975943c319eace72cf686a2d3dc3b7db151799c8
github username <github email address>
2020-01-17 19:01:32 +0000

    crud

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,6 @@
 const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const MongoClient = mongodb.MongoClient
+const {MongoClient, ObjectID} = mongodb
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -8,12 +9,83 @@ MongoClient.connect(connectionURL,{useUnifiedTopology: true}, (error, client) =>
     if (error){
         return console.log('Unable to connect')
     }
+
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'haider',
-        age: 24
+ db.collection('users').deleteMany({ name: 'haider'}).then((result) => {
+        console.log('sucess', result)
+    }).catch((error) => {
+        console.log("error", error)
     })
-})
+    
+}) 
+       
 
-//useNewUrlParser: true
+
+    // //"you can aalso chain .then directly. no need for const" 
+    // const updatePromise = db.collection('users').updateOne({
+    //     _id: new ObjectID("5e21d2d18e57cb4010f91c90")
+    // }, {
+    //     $set: {
+    //         name: 'Mike'
+    //     },
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }
+    // )
+    // updatePromise.then((result) => {
+    //     console.log('sucess', result)
+    // }).catch((error) => {
+    //     console.log("error", error)
+    // })
+    //   })
+
+
+
+    // db.collection('users').find({ name: 'haider'}).toArray((error,users) =>{
+    //     if (error) {
+    //         return console.log('unable fetch')
+    //     }
+    //     console.log(users)
+    // }
+    // )
+    // })
+
+   // //{ _id: new ObjectID("5e21d2d18e57cb4010f91c90")}
+// db.collection('users').findOne({ name: 'haider'},
+// (error,user) =>{
+//     if (error) {
+//         return console.log('unable fetch')
+//     }
+//     console.log(user)
+// }
+// )
+// })
+
+//     db.collection('users').insertOne({
+//         name: 'haider',
+//         age: 24
+//     })
+
+//     db.collection('users').insertMany([
+//     {
+//         name: 'haider',
+//         age: 24
+//     },
+//     {
+//         name: 'shoul',
+//         age: 25
+//     }
+//     ], (error, result) => {
+//         if (error) {
+//             return console.log("errror")
+//         }
+//         console.log(result.ops)
+//     })
+// })
+// //useNewUrlParser: true
+
+
+
+
