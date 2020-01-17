2847639ac5ac11b3cced84cc2ad3ba84f55d4dbf
github username <github email address>
2020-01-17 15:33:49 +0000

    mongo db setup

---
+++ task-manager/mongodb.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL,{useUnifiedTopology: true}, (error, client) => {
+    if (error){
+        return console.log('Unable to connect')
+    }
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'haider',
+        age: 24
+    })
+})
+
+//useNewUrlParser: true
